a8710104bfbe6a1dce91b014d0fcb091567418e9
Zhenkaaf <zhenkaaf@gmail.com>
2023-03-11 17:26:10 +0200

    hh

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongoose
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Origin');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
   });
