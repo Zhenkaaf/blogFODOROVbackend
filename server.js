30126081d6335b060300b8b228b8565b1b27f2a8
Zhenkaaf <zhenkaaf@gmail.com>
2023-03-11 16:37:47 +0200

    test4

--- server.js
+++ server.js
@@ -19,13 +19,21 @@ mongoose
     .then((res) => console.log('connected to db'))
     .catch((error) => console.log('error db'));
 
-app.use(function (req, res, next) {
+/* app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-Width, content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-});
+}); */
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 
 app.use(express.json());
 /* app.use(bodyParser.json());
